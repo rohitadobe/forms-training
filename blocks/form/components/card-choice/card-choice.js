eb223a3f922736d1f0075d20a9ed1912b0fe84e3
rohitadobe <inf00744@adobe.com>
2026-08-06 11:51:30 +0530

    commit

--- blocks/form/components/card-choice/card-choice.js
+++ blocks/form/components/card-choice/card-choice.js
@@ -10,13 +10,14 @@
  * @param {HTMLElement} parentElement - The parent container element of the field.
  * @param {string} formId - The unique identifier of the form.
  */
-export default async function decorate(fieldDiv, fieldJson, parentElement, formId) {
-  console.log('⚙️ Decorating cards component:', fieldDiv, fieldJson, parentElement, formId);
+import { createOptimizedPicture } from '../../../../scripts/aem.js';
 
-  // TODO: Implement your custom component logic here
-  // You can access the field properties via fieldJson.properties
-  // You can access the parent container via parentElement
-  // You can access the form ID via formId
 
-  return fieldDiv;
+export default function decorate(element, fieldJson, container, formId) {
+    element.classList.add('card');
+    element.querySelectorAll('.radio-wrapper').forEach((radioWrapper) => {
+        const image = createOptimizedPicture('https://main--afb--jalagari.hlx.live/lab/images/card.png', 'card-image');
+        radioWrapper.appendChild(image);
+    });
+    return element;
 }
