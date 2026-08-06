7aa9d591514c87a483a8855e5717148843c837c5
rohitadobe <inf00744@adobe.com>
2026-08-06 11:26:37 +0530

    commit

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,24 +1,15 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
-export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+import { createOptimizedPicture } from '../../../../scripts/aem.js';
+
+
+export default function decorate(element, fieldJson, container, formId) {
+    element.classList.add('card');
+    element.querySelectorAll('.radio-wrapper').forEach((radioWrapper) => {
+        const image = createOptimizedPicture('https://main--afb--jalagari.hlx.live/lab/images/card.png', 'card-image');
+        radioWrapper.appendChild(image);
     });
-    ul.append(li);
-  });
-  ul.querySelectorAll('picture > img').forEach((img) => {
-    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-    moveInstrumentation(img, optimizedPic.querySelector('img'));
-    img.closest('picture').replaceWith(optimizedPic);
-  });
-  block.textContent = '';
-  block.append(ul);
+    return element;
 }
+
