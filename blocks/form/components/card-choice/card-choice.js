17fdf575c83410b63ce641cc46f08f5ebcfb82cc
rohitadobe <inf00744@adobe.com>
2026-08-06 11:54:26 +0530

    commit

--- blocks/form/components/card-choice/card-choice.js
+++ blocks/form/components/card-choice/card-choice.js
@@ -1,3 +1,15 @@
+/**
+ * Custom cards component
+ * Based on: Radio Group
+ */
+
+/**
+ * Decorates a custom form field component
+ * @param {HTMLElement} fieldDiv - The DOM element containing the field wrapper.
+ * @param {Object} fieldJson - The form json object for the component.
+ * @param {HTMLElement} parentElement - The parent container element of the field.
+ * @param {string} formId - The unique identifier of the form.
+ */
 import { createOptimizedPicture } from '../../../../scripts/aem.js';
 
 
