e0e4e30b0f8e0ca9764478f0daa10e3040f5edbd
rohitadobe <inf00744@adobe.com>
2026-08-06 12:16:58 +0530

    commit

--- blocks/form/components/card-choice/card-choice.js
+++ blocks/form/components/card-choice/card-choice.js
@@ -16,7 +16,7 @@ import { createOptimizedPicture } from '../../../../scripts/aem.js';
 export default function decorate(element, fieldJson, container, formId) {
     element.classList.add('card');
     element.querySelectorAll('.radio-wrapper').forEach((radioWrapper) => {
-        const image = createOptimizedPicture('https://main--afb--jalagari.hlx.live/lab/images/card.png', 'card-image');
+        const image = createOptimizedPicture('https://main--afb--jalagari.hlx.live/lab/images/card.png','card-image');
         radioWrapper.appendChild(image);
     });
     return element;
